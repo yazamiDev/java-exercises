b25a77100fa7e5723e4b59736fc3e394457925f9
Yousif Azami <yousifazami@outlook.com>
2020-09-08 15:15:47 -0700

    completed exercises 10.11, 10.12

---
+++ chapter10/MarkLength4.java
@@ -0,0 +1,16 @@
+/**
+ * Write a method markLength4 that takes an ArrayList of Strings as a 
+ * parameter and that places a string of four asterisks "****" in front of 
+ * every string of length 4.
+ */
+
+public void markLength4(ArrayList<String> list) {
+    for(int i=0; i<list.size(); i++) {
+        String temp = list.get(i);
+        
+        if(temp.length() == 4) {
+            list.add(i, "****");
+            i++;
+        }
+    }
+}
