e5fe1e3faf57f98f7ea07481f8c6a6546d4fe7ce
Yousif Azami <yousifazami@outlook.com>
2020-09-11 21:10:11 -0700

    completed exercise 12.6

---
+++ chapter12/WriteSquares.java
@@ -0,0 +1,25 @@
+/**
+ * Write a method writeSquares that accepts an integer 
+ * parameter n and prints the first n squares separated 
+ * by commas, with the odd squares in descending order 
+ * followed by the even squares in ascending order.
+ */
+
+public void writeSquares(int n) {
+    if(n<1) {
+        throw new IllegalArgumentException();
+    }
+    
+    if(n == 1) {
+        System.out.print("1");
+        return;
+    }
+    
+    if(n % 2 == 0) {
+        writeSquares(n-1);
+        System.out.print(", " + n * n);
+    } else {
+        System.out.print(n * n + ", ");
+        writeSquares(n-1);
+    }
+}
