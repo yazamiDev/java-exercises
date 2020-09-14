9b5bb90b94b3f27a58ccfaf482bff5ac59854e23
Yousif Azami <yousifazami@outlook.com>
2020-09-13 23:43:34 -0700

    started exercise 12.12

---
+++ chapter12/IsReverse.java
@@ -0,0 +1,17 @@
+/**
+ * rite a recursive method isReverse that accepts two strings as a 
+ * parameter and returns true if the two strings contain the same 
+ * sequence of characters as each other but in the opposite order 
+ * (ignoring capitalization), and false otherwise. For example, the 
+ * string "hello" backwards is "olleh", so a call of isReverse("hello", 
+ * "olleh") would return true. Since the method is case-insensitive, 
+ * you would also get a true result from a call of isReverse("Hello", 
+ * "oLLEh"). The empty string, as well as any one-letter string, is 
+ * considered to be its own reverse. The string could contain 
+ * characters other than letters, such as numbers, spaces, or other 
+ * punctuation; you should treat these like any other character. 
+ * The key aspect is that the first string has the same sequence 
+ * of characters as the second string, but in the opposite order, 
+ * ignoring case. 
+ */
+
