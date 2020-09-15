9c2228c3a20e3a1b21aef6a2e58c53a48e95e380
Yousif Azami <yousifazami@outlook.com>
2020-09-14 23:39:13 -0700

    completed exercise 12.21

---
+++ chapter12/CountBinary.java
@@ -0,0 +1,24 @@
+/**
+ * Write a method countBinary that accepts an integer n as a 
+ * parameter and that prints all binary numbers that have n 
+ * digits in ascending order, printing each value on a separate 
+ * line. All n digits should be shown for all numbers, including 
+ * leading zeros if necessary. You may assume that n is non-negative. 
+ * If n is 0, a blank line of output should be produced. Do 
+ * not use a loop in your solution; implement it recursively.
+ */
+
+public void countBinary(int n) {
+    String num = "";
+    countBinary(n, num);
+}
+
+public void countBinary(int n, String num) {
+    if(n == 0) {
+        System.out.println(num);
+        return;
+    }
+    
+    countBinary(n - 1, num + "0");
+    countBinary(n - 1, num + "1");
+}
