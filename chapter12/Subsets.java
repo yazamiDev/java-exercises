d01ea1f1e46db204a21d21188a07c9ea0b48eabc
Yousif Azami <yousifazami@outlook.com>
2020-09-14 23:41:22 -0700

    started exercise 12.22

---
+++ chapter12/Subsets.java
@@ -0,0 +1,10 @@
+/**
+ * Write a method subsets that uses recursive backtracking 
+ * to find every possible sub-list of a given list. A 
+ * sub-list of a list L contains 0 or more of L's elements. 
+ * Your method should accept a List of strings as its parameter 
+ * and print every sub-list that could be created from elements 
+ * of that list, one per line.
+ */
+
+ 
