0c6ffe8db19dbbf9668a45620c21fdf611042b75
Yousif Azami <yousifazami@outlook.com>
2020-09-14 23:35:34 -0700

    completed exercise 12.13

--- chapter12/IndexOf.java
+++ chapter12/IndexOf.java
@@ -6,3 +6,13 @@
  * method and their expected return values. Notice that case matters, 
  * as in the last example that returns -1.
  */
+
+public int indexOf(String str1, String str2) {
+    if(str1.length() < str2.length()) return -1;        
+    if(str1.substring(0, str2.length()).equals(str2)) return 0;
+        
+    int index = indexOf(str1.substring(1), str2);
+    if(index == -1) return -1;
+     
+    return 1 + index;
+}
