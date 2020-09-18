70192b2c02edf698226d874c8b887ada1ee8e081
Yousif Azami <yousifazami@outlook.com>
2020-09-18 12:10:44 -0700

    completed exercise 16.9

---
+++ chapter16/Stutter.java
@@ -0,0 +1,17 @@
+/**
+ * Write a method stutter that doubles the size of a list by replacing
+ *  every integer in the list with two of that integer. 
+ */
+
+public void stutter() {
+    if(front == null) {
+        return;
+    }
+    
+    ListNode current = front;
+    while(current != null) {
+        ListNode n = new ListNode(current.data, current.next);
+        current.next = n;
+        current = n.next;
+    }
+}
