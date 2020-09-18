a6d5fd73794c525ec9909883041c7d98cb157a24
Yousif Azami <yousifazami@outlook.com>
2020-09-18 07:43:57 -0700

    completed exercise 12.22

--- chapter12/Subsets.java
+++ chapter12/Subsets.java
@@ -7,4 +7,18 @@
  * of that list, one per line.
  */
 
- 
+public void subsets(List<String> list) {
+    subsets(list, 0, list.size());
+}
+
+public void subsets(List<String> list, int start, int rem) {
+    if(rem == 0) {
+        System.out.println(list);
+        return;
+    }
+    
+    subsets(list, start + 1, rem - 1);
+    String str = list.remove(start);
+    subsets(list, start, rem - 1);
+    list.add(start, str);
+}
