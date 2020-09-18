755779911902c810db86f108cc9edf83312b6835
Yousif Azami <yousifazami@outlook.com>
2020-09-17 22:04:47 -0700

    completed exercise 14.11

---
+++ chapter14/Reorder.java
@@ -0,0 +1,31 @@
+/**
+ * Write a method reorder that takes a queue of integers as a 
+ * parameter and that puts the integers into sorted (nondecreasing) 
+ * order assuming that the queue is already sorted by absolute value.
+ */
+
+public void reorder(Queue<Integer> q) {
+    Stack<Integer> s = new Stack<Integer>();
+    int size = q.size();
+    int count = 0;
+    
+    for(int i=0; i<size; i++) {
+        int n = q.remove();
+        
+        if(n < 0) {
+            s.push(n);
+        } else {
+            q.add(n);
+            count++;
+        }
+    }
+    
+    while(!s.isEmpty()) {
+        q.add(s.pop());
+    }
+    
+    for(int i=0; i<count; i++) {
+        q.add(q.remove());
+    }
+    
+}
