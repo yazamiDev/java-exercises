4aaee620894f98be36129d2030e9d0f94d2ed422
Yousif Azami <yousifazami@outlook.com>
2020-09-22 23:54:44 -0700

    completed exercise 17.4

---
+++ chapter17/CountEvenBranches.java
@@ -0,0 +1,24 @@
+/**
+ * Write a method countEvenBranches that returns the number of 
+ * branch nodes in a binary tree that contain even numbers. A 
+ * branch node is one that has one or two children (i.e., not 
+ * a leaf node). An empty tree has 0 even branches.
+ */
+
+public int countEvenBranches() {
+    return countEvenBranches(overallRoot);
+}
+
+public int countEvenBranches(IntTreeNode node) {
+    if(node == null) {
+        return 0;
+    }
+    
+    int count = 0;
+    
+    if((node.left != null || node.right != null) && node.data % 2 == 0) {
+        count++;
+    }
+    
+    return count + countEvenBranches(node.left) + countEvenBranches(node.right);
+}
